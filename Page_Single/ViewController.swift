588629028f17b07b0870d178cb59fb156c15380f
mjkobaya <mkobayashi@Ms-MacBook-Pro.local>
2014-11-07 21:12:51 -0800

    Initial commit

---
+++ Page_Single/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Page_Single
+//
+//  Created by M Kobayashi on 11/7/14.
+//  Copyright (c) 2014 Team 3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
