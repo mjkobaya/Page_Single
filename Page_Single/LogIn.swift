df3722d4a8a81973442850c47a3232ce4b47b914
mjkobaya <mkobayashi@Ms-MacBook-Pro.local>
2014-11-10 10:13:36 -0800

    Added LogIn.swift

---
+++ Page_Single/LogIn.swift
@@ -0,0 +1,13 @@
+//
+//  LogIn.swift
+//  Page_Single
+//
+//  Created by M Kobayashi on 11/9/14.
+//  Copyright (c) 2014 Team 3. All rights reserved.
+//
+
+import UIKit
+
+class LogIn: NSObject {
+   
+}
