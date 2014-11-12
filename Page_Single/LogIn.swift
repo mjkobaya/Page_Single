25bbf63d7b508a49012b89a9bc3aaebec3e1daa7
Mish <mjkobaya@gmail.com>
2014-11-11 21:45:27 -0800

    Log in control flow implemented

--- Page_Single/LogIn.swift
+++ Page_Single/LogIn.swift
@@ -2,6 +2,8 @@
 //  LogIn.swift
 //  Page_Single
 //
+//  Class specifying a LogIn object to create a log in session
+//
 //  Created by M Kobayashi on 11/9/14.
 //  Copyright (c) 2014 Team 3. All rights reserved.
 //
@@ -9,5 +11,32 @@
 import UIKit
 
 class LogIn: NSObject {
+    
+    let username: String
+    let password: String
+    
+    init(username: String, password: String)
+    {
+        self.username = username
+        self.password = password
+    }
+    
+    func match() -> Int
+    {
+        // Check that username and password given match the ones on the database
+        
+        // Contact database and retrieve user info
+        
+        // Check that they match
+        if self.username == "Melinda" && self.password == "password"
+        {
+            return 1
+        }
+        
+        else
+        {
+            return 0
+        }
+    }
    
 }
--- Page_Single/ViewController.swift
+++ Page_Single/ViewController.swift
@@ -11,11 +11,49 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var status = 0
     @IBAction func logInButton(sender: UIButton) {
         
         println(usernameTextField.text)
+        println(passwordTextField.text)
         
+        // On Log In button click check database to see if user name
+        // and password match
+        // For now, hard code
+        
+        let logInSession = LogIn(username: usernameTextField.text, password: passwordTextField.text)
+        
+        // If username and password match, segue into navigation controller
+        if logInSession.match() == 1
+        {
+            println("inside if statement")
+            self.status = 1
+            shouldPerformSegueWithIdentifier("logInSegue", sender: sender)
+        }
+        
+        // Else, update with "Username or password are incorrect"
+        else
+        {
+            println("Username and password did not match.")
+            shouldPerformSegueWithIdentifier("logInSegue", sender: sender)
+        }
     }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool
+    {
+        if self.status == 1
+        {
+            return true
+        }
+        
+        else
+        {
+            return false
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
