0d39cf1a33cf5cfc063c46bcf3ba6bd3dc2290f8
Mish <mjkobaya@gmail.com>
2014-11-14 00:49:37 -0800

    [STABLE] Added Database class

---
+++ Page_Single/Database.swift
@@ -0,0 +1,13 @@
+//
+//  Database.swift
+//  Page_Single
+//
+//  Created by M Kobayashi on 11/14/14.
+//  Copyright (c) 2014 Team 3. All rights reserved.
+//
+
+import UIKit
+
+class Database: NSObject {
+   
+}
